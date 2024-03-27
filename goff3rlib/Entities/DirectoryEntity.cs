9a6e135396418f6720dd58309a2a9c59219dcfb0
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-27 23:23:57 +0100

    Type update

--- goff3rlib/Entities/DirectoryEntity.cs
+++ goff3rlib/Entities/DirectoryEntity.cs
@@ -35,6 +35,8 @@ DigitSeq  ::= digit {digit}.
 Port      ::= DigitSeq.
  */
 
+using System.Runtime.CompilerServices;
+
 namespace goff3rlib.Entities
 {
     internal sealed class DirectoryEntity
@@ -51,28 +53,37 @@ namespace goff3rlib.Entities
            7   Item is an Index-Search server.
            8   Item points to a text-based telnet session.
            9   Item is a binary file!
-        */
-        public enum Type {  
-                            GopherFile = 0,
-                            GopherDirectory = 1,
-                            GopherCSOPhoneBookServer = 2,
-                            GopherError = 3,
-                            GopherBixHexedMacintoshFile = 4,
-                            GopherDOSBinaryArchive = 5,
-                            GopherUNIXuuencodedFile = 6,
-                            GopherIndexSearchServer = 7,
-                            GopherToTelnetSessionPointer= 8,
-                            GopherBinaryFile = 9
-                        }
+               Client must read until the TCP connection closes.  Beware.
+           +   Item is a redundant server
+           T   Item points to a text-based tn3270 session.
+           g   Item is a GIF format graphics file.
+           I   Item is some kind of image file.  Client decides how to display.
 
-        private readonly string CRLF = "\r\n";
-        private readonly string TAB = "\t";
-        private readonly string Lastline = string.Empty;
-        private readonly string NUL = "\0";
+        */
+
+       private const char GopherFile = '0';
+       private const char GopherDirectory = '1';
+       private const char GopherCSOPhoneBookServer = '2';
+       private const char GopherError = '3';
+       private const char GopherBixHexedMacintoshFile = '4';
+       private const char GopherDOSBinaryArchive = '5';
+       private const char GopherUNIXuuencodedFile = '6';
+       private const char GopherIndexSearchServer = '7';
+       private const char GopherToTelnetSessionPointer = '8';
+       private const char GopherBinaryFile = '9';
+       private const string Gopher_CRLF = "\r\n";
+       private const string Gopher_TAB = "\t";
+       private const string Gopher_Lastline = "." + Gopher_CRLF;
+       private const char Gopher_NUL = '\0';
+       private const char GopherRedundantServer = '+';
+       private const char GopherTN3270Session = 'T';
+       private const char GopherGIFfile = 'g';
+       private const char GopherImageFile = 'I';
+
 
         private DirectoryEntity()
         {
-            Lastline = string.Concat('.', CRLF);
+
         }
 
         private string ToString() { return string.Empty; }
