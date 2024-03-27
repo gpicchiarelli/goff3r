d9890c1cef3d5d98589d5a9d65b9c597afde2e9f
Giacomo Picchiarelli <gpicchiarelli@gmail.com>
2024-03-27 23:52:00 +0100

    Updated entity

--- goff3rlib/Entities/DirectoryEntity.cs
+++ goff3rlib/Entities/DirectoryEntity.cs
@@ -35,9 +35,6 @@ DigitSeq  ::= digit {digit}.
 Port      ::= DigitSeq.
  */
 
-using System.Runtime.CompilerServices;
-using static goff3rlib.Entities.GopherType;
-
 namespace goff3rlib.Entities
 {
 
@@ -47,15 +44,23 @@ namespace goff3rlib.Entities
         private const string Gopher_TAB = "\t";
         private const string Gopher_Lastline = "." + Gopher_CRLF;
         private const char Gopher_NUL = '\0';
-        private Uri GopherHost = null;
-        private int port = 70;
+        private readonly Uri? GopherHost = null;
+        private readonly int port = 70;
         private const char GopherTypeChar = GopherType.GopherDirectory;
+        private readonly string Selector = string.Empty; //255 chars
+        private readonly string User_Name = string.Empty; //69 chars
 
-        private DirectoryEntity()
+        private DirectoryEntity(string selector, string user_Name, Uri Host, int port = 70)
         {
-
+            Selector = selector[..254];
+            User_Name = user_Name[..69];
+            GopherHost = Host;
+            //Type User_Name Tab Selector Tab Host Tab Port CR-LF
         }
 
-        private string ToString() { return string.Empty; }
+        private string ToString()
+        {
+            return GopherTypeChar + User_Name + Gopher_TAB + Selector + Gopher_TAB + GopherHost + Gopher_TAB + port + Gopher_CRLF;
+        }
     }
 }
